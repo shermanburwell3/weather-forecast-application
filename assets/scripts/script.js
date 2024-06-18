3c3077822ba9829ef943f760a3da35a3ded2f85f
Sherman Burwell <shermanburwell3@gmail.com>
2024-06-18 12:51:21 -0500

    Added geolocation api and code to parse lat and lon by city name

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,55 @@
-const cityName = "Austin";
-const apiKey = "12341234123412341234123412341234";
-const queryUrl = `api.weathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+let cityName = "Austin";
+let stateCode = "TX";
+let countryCode = "US"
+let lat;
+let lon;
+const apiKey = "0c1d7915ad2662f0e450b432130b6989";
+const forecastQueryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+const geoQueryUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${apiKey}`
 
-fetch(queryUrl);
+
+
+// Read what we get back, look at the documentation
+
+// Create function for fetching the data
+
+function fetchWeather(query, city, state, country) {
+
+    cityName = city;
+    stateCode = state;
+    countryCode = country;
+    fetch(query)
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+            .then(function (data) {
+                console.log(data);
+                lat = data[0].lat;
+                lon = data[0].lon;
+                console.log(lat, lon);
+            });
+
+
+}
+
+// Create function for search submit, add search to localStorage
+
+function search() {
+
+
+
+}
+
+// Create function for creating current weather card (may use dayjs)
+
+// Create function for creating forecast weather cards
+
+// Create function for rendering search history, only allow a certain amount into the history
+
+// Create function to convert city to lat and long to plug into forecast api
+
+// Event Listener for search submit
+
+
+fetchWeather(geoQueryUrl, cityName, stateCode, countryCode);
